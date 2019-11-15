98c86633845b63a4f9549f0104b1383ab4e6ebbb
Pratiquea <prateekarorav2@gmail.com>
2019-11-15 18:09:29 -0500

    added trajectory prediction boiler plate

---
+++ traj_predict.py
@@ -0,0 +1,24 @@
+# !usr/bin/env python
+
+import rospy
+import numpy
+from nav_msgs.msg import Odometry
+
+class EVDodge
+	def __init__:
+		self.object_sub = rospy.Subscriber('/evdodge_demo_ball/odom',Odometry,object_sub_cb )
+		self.object_odom =  Odometry()
+
+	def object_sub_cb(self,data):
+		self.object_odom = data
+
+	def predict_traj():
+
+
+def main():
+	EV_obj = EVDodge()
+
+
+
+if __init__ == '__main__':
+	main()
